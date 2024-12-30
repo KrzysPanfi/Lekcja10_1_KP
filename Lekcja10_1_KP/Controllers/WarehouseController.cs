85c99c078ae9da58411e5188286d68daa78b63cd
Krzysztof <krzyspanfilewicz@gmail.com>
2024-12-30 19:07:07 +0100

    Zmieniono kod na jedna supermetode

--- Lekcja10_1_KP/Controllers/WarehouseController.cs
+++ Lekcja10_1_KP/Controllers/WarehouseController.cs
@@ -14,29 +14,18 @@ namespace lekcja10_KP_1.Controllers
     [ApiController]
     public class WarehouseController : ControllerBase
     {
-       // [HttpGet]
+       [HttpGet]
         public IActionResult Get()
         {
-            var dane = new _2019sbdContext().Warehouses;
+            var dane = new _2019sbdContext().Orders;
             return Ok(dane);
         }
-       // [HttpGet("Warehouse")]
-        public bool IsWarehouseindb(int id)
+        [HttpPost("AddOrder")]
+        public IActionResult AddOrder(int Idproduct, int IdWarehouse, int amount, DateTime createdAt)
         {
-            var dane = new _2019sbdContext().Warehouses.Where(w => w.IdWarehouse == id).Any();
-            return dane;
-        }
-      //  [HttpGet("Product")]
-        public bool IsProductindb(int id)
-        {
-            var dane = new _2019sbdContext().Products.Where(p => p.IdProduct == id).Any();
-            return dane;
-        }
-        [HttpPost]
-        public IActionResult AddOrder(int Idproduct, int IdWarehouse, int amount, DateTime CreatedAt)
-        {
-            var isproduct = IsProductindb(Idproduct);
-            var iswarehouse = IsWarehouseindb(IdWarehouse);
+            var context = new _2019sbdContext();
+            var isproduct = context.Products.Where(p => p.IdProduct == Idproduct).Any();
+            var iswarehouse = context.Warehouses.Where(w => w.IdWarehouse == IdWarehouse).Any();
             if (!isproduct)
             {
                 return BadRequest("Produkt nie istnieje");
@@ -49,43 +38,46 @@ namespace lekcja10_KP_1.Controllers
             {
                 return BadRequest("Zła ilość");
             }
-            var orderid = IsOrder(Idproduct, amount, CreatedAt);
+            var orderid = -11;
+            var order = context.Orders.Where(o => o.IdProduct == Idproduct && o.Amount == amount && o.CreatedAt < createdAt).SingleOrDefault();
+            if (!order.Equals(null))
+            {
+                order.FulfilledAt = DateTime.Now;
+                orderid = order.IdOrder;
+            }
+            orderid = -1;
+
             if (orderid < 0)
             {
                 return BadRequest("Zamówienie nie istnieje");
             }
-            if (!Iscompleted(orderid))
+            var iscompleted = context.ProductWarehouses.Where(pw => pw.IdOrder == orderid).Any();
+            if (!iscompleted)
             {
                 return BadRequest("Zamówienie nie zostało zrealizowane");
             }
-            Update(orderid);
-            return Ok(Insert(IdWarehouse,Idproduct,orderid,amount));
-        }
-        public int IsOrder(int id, int amount, DateTime createdAt)
-        {
-            
-                var dane = new _2019sbdContext().Orders.Where(o => o.IdProduct == id && o.Amount == amount && o.CreatedAt < createdAt).SingleOrDefault();
-            if (!dane.Equals(null)){ 
 
-                return dane.IdOrder;
+            ProductWarehouse insert = new ProductWarehouse();
+            var product = context.Products.Where(p => p.IdProduct == Idproduct).SingleOrDefault();
+            if (!product.Equals(null))
+            {
+                decimal price = product.Price * amount;
+
+                insert.IdProduct = Idproduct;
+                insert.IdWarehouse = IdWarehouse;
+                insert.Amount = amount;
+                insert.CreatedAt = DateTime.Now;
+                insert.Price = price;
+                insert.IdOrder = orderid;
+                context.ProductWarehouses.Add(insert);
             }
-                return -1;
+            context.SaveChanges();
+
+            return Ok(insert.IdProductWarehouse);
+
         }
- 
-        public bool Iscompleted(int id)
-        {
-            var dane = new _2019sbdContext().ProductWarehouses.Where(pw => pw.IdOrder == id).Any();
-            return dane;
-        }
-        public  void Update(int Orderid)
-        { var context = new _2019sbdContext();
-                var dane = context.Orders.Where(o => o.IdOrder == Orderid).SingleOrDefault();
-            if (dane != null){
-            dane.FulfilledAt = DateTime.Now;
-                context.SaveChanges();
-            }
-        }
-        public int Insert(int Warehouseid, int Productid, int Orderid, int Amount)
+      
+    /*    public int Insert(int Warehouseid, int Productid, int Orderid, int Amount)
         {
             var context = new _2019sbdContext();
             ProductWarehouse insert = new ProductWarehouse();
@@ -105,6 +97,6 @@ namespace lekcja10_KP_1.Controllers
                 return insert.IdProductWarehouse;
             }
             return -1;
-        }
+       */ }
     }
-}
+
